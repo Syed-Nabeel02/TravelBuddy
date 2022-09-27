aa1468db0756915375e80c555ab4a31ef6ad37b7
Syed Nabeel <nabeelsyed234@gmail.com>
2022-09-27 16:25:15 -0400

    Enter

---
+++ src/Components/Map/Map.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
+
+import useStyles from "./styles.js";
+
+const Map = ({ setCrd, setBounds, crd, places, setChildClicked }) => {
+  const isDesktop = useMediaQuery("(max-width:600px)");
+  const classes = useStyles();
+
+  //const crd = {lat:0,lng:0}
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBFM_HEr22oAzOD8KNlvd6AtrMlHkW1KUs" }}
+        defaultCenter={crd}
+        defaultZoom={14}
+        center={crd}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={(e) => {
+          console.log(e);
+          //the onchange property gives us the coords whenmap changes
+          setCrd({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {isDesktop ? (
+              <LocationOnOutlinedIcon color="primary" fontsize="large" />
+            ) : (
+              <Paper elevation={6} className={classes.paper}>
+                <Typography className={classes.Typography} variant="subtitle1">
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                  alt={place.name}
+                />
+                <Rating
+                  size="small"
+                  value={Number(place.rating)}
+                  readOnly
+                ></Rating>
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
